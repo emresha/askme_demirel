3397b9659eb5d0cc631e8cd39912794c97a1ebbd
Emre Demirel <emredemirel2020@yandex.ru>
2024-10-28 16:21:11 +0300

    all routing done, only pagination remains

--- app/views.py
+++ app/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
+from django.http import Http404
 from askme_demirel.settings import BASE_DIR
+from random import choice, randint
 
 # Create your views here.
 
+tags = ["Urgent", "Philosophy", "Games", "Physics", "Programming", "Common"]
+
+questions = []
+for i in range(1,30):
+  questions.append({
+    'title': 'title ' + str(i),
+    'id': i,
+    'text': 'text' + str(i),
+    'tags': [choice(tags), choice(tags)],
+    'likes': randint(-100, 100)
+  })
+
+
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", context={'questions': questions[:10]})
+
+def hot(request):
+   return render(request, "index.html", context={'questions': sorted(questions[:10],
+                                    key=lambda x: x['likes'], reverse=True), 'hot': True})
+
+def tag(request, tag_name):
+    tag_questions = list(filter(lambda x: tag_name.title() in x['tags'], questions))
+    if not tag_questions:
+        raise Http404("Tag not found")
+    return render(request, "tag.html", context={'questions': tag_questions, 'tag': tag_name})
+
+def ask(request):
+    return render(request, "ask.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+def question(request, id):
+    try:
+        question = questions[id - 1]
+    except IndexError:
+        raise Http404("Question not found.")
+    return render(request, "question.html", context={'question': question, 'id': id})
+
+def settings(request):
+    return render(request, "settings.html")
--- askme_demirel/urls.py
+++ askme_demirel/urls.py
@@ -19,5 +19,12 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('hot/', views.hot, name="hot"),
+    path('tag/<str:tag_name>', views.tag, name="tag"),
+    path('ask/', views.ask, name="ask"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('question/<int:id>', views.question, name="question"),
+    path('settings/', views.settings, name="settings")
 ]
